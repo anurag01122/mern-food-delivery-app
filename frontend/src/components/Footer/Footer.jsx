fdc57b699916509f4f4b7a23b4087efe9814cb06
Anurag Kumar <anuragkumar65337@gmail.com>
2025-12-29 23:30:12 +0530

    Footer update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,36 +6,46 @@ const Footer = () => {
   return (
     <div className='footer' id='footer'>
         <div className="footer-content">
+
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum in, beatae dolorem non optio cupiditate, quam sunt dicta dolores minima exercitationem ducimus totam aut asperiores inventore harum laudantium. Distinctio, libero.</p>
+                <h1 className="footer-logo">Food Express</h1>
+
+                <p>
+                  Fast and fresh food delivered to your doorstep.
+                  Order from your favorite restaurants anytime.
+                </p>
+
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <img src={assets.facebook_icon} alt="Facebook" />
+                    <img src={assets.twitter_icon} alt="Twitter" />
+                    <img src={assets.linkedin_icon} alt="LinkedIn" />
                 </div>
             </div>
+
             <div className="footer-content-center">
                 <h2>COMPANY</h2>
                 <ul>
                     <li>Home</li>
-                    <li>About us</li>
+                    <li>About Us</li>
                     <li>Delivery</li>
                     <li>Privacy Policy</li>
                 </ul>
             </div>
+
             <div className="footer-content-right">
                 <h2>GET IN TOUCH</h2>
                 <ul>
-                    <li>+94 765489545</li>
-                    <li>dulanjalisenarathna93@gmail.com</li>
+                    <li>+91 9876543210</li>
+                    <li>support@foodexpress.com</li>
                 </ul>
             </div>
-           
+
         </div>
+
         <hr />
+
         <p className="footer-copyright">
-            Copyright 2024 &copy; Dulanjali - All Right Reserved.
+            © 2024 Food Express. All rights reserved.
         </p>
     </div>
   )
